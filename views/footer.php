c2006f4046ef28d0040c65fffb55b32ad0dad5c2
Yasmina Achahbar <yasmina.achahbar2006@gmail.com>
2026-04-23 16:39:40 +0200

    añadir estructura modular con header y footer

---
+++ views/footer.php
@@ -0,0 +1,7 @@
+<hr>
+<footer>
+    <p>Proyecto TFC - 2026</p>
+    <p><a href="#">GitHub</a> | <a href="#">LinkedIn</a></p>
+</footer>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Backlog Killer</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<header>
+    <nav>
+        <strong>Backlog Killer</strong>
+        <a href="index.php">Inicio</a>
+        <a href="login.php">Login</a>
+        <a href="registro.php">Registro</a>
+    </nav>
+</header>
+<hr>
