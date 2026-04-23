c5e660c07334bcfd3827ed736a7016b778dc07af
Yasmina Achahbar <yasmina.achahbar2006@gmail.com>
2026-04-23 17:32:57 +0200

    Crear carpeta de assets y página de inicio

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php 
+    include '../views/header.php'; 
+?>
+
+<main>
+    <h1>¡DEJA DE PERDER EL TIEMPO DECIDIENDO QUÉ JUGAR!</h1>
+    <p>Tu ruleta personalizada para liquidar juegos pendientes.</p>
+    <a href="registro.php">EMPIEZA AHORA</a>
+</main>
+
+<?php 
+    include '../views/footer.php'; 
+?>
