f71f8b6ff353d7153e242cc77d1ce450c2a48933
Yasmina Achahbar <yasmina.achahbar2006@gmail.com>
2026-05-06 21:18:37 +0200

    implementación estilos footer y body

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,12 @@
 <hr>
-<footer>
-    <p>Proyecto TFC - 2026</p>
-    <p><a href="#">GitHub</a> | <a href="#">LinkedIn</a></p>
+<footer class="site-footer">
+    <div class="footer-content">
+        <p>Backlog Killer &copy; <?= date("Y"); ?> - Proyecto TFC</p>
+        <div class="footer-links">
+            <a href="https://github.com" target="_blank">GitHub</a>
+            <a href="https://linkedin.com" target="_blank">LinkedIn</a>
+        </div>
+    </div>
 </footer>
 </body>
 </html>
