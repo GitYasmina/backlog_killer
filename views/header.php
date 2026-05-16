82cdc9471ae333eb02994fe5643471c390a4afce
Yasmina Achahbar <yasmina.achahbar2006@gmail.com>
2026-05-16 18:03:43 +0200

    crear menu dinamico segun la sesion

--- views/header.php
+++ views/header.php
@@ -7,14 +7,25 @@
 <body>
 <header class="main-header">
     <div class="header-container">
-        <div class="logo"><a href="index.php"><strong>Backlog Killer</strong></a></div>
+        <div class="logo">
+            <a href="<?= isset($_SESSION['user_id']) ? 'dashboard.php' : 'index.php' ?>">
+                <strong>Backlog Killer</strong>
+            </a>
+        </div>
+        
         <nav class="nav-menu">
             <ul>
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="login.php">Login</a></li>
-                <li><a href="registro.php">Registro</a></li>
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    <li><a href="dashboard.php">Biblioteca</a></li>
+                    <li><a href="buscar_juego.php">Buscar</a></li>
+                    <li><a href="ruleta.php">Ruleta</a></li>
+                    <li><a href="logout.php">Salir</a></li>
+                <?php else: ?>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="registro.php">Registro</a></li>
+                <?php endif; ?>
             </ul>
         </nav>
     </div>
 </header>
-<hr>
